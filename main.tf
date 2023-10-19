659f68461b32df84db45a974deb00ae4940e6cd2
diasaini <148217634+diasaini@users.noreply.github.com>
2023-10-19 10:39:44 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+variable "var1" {
+    type = list
+    default = ["myresgrp1","myresgrp2","myresgrp3"]
+  
+}
+
+output "outvars1" {
+
+    value = [for i in var.var1 : upper(i)]
+  
+}
+
+output "outvars2" {
+
+value = [for i in var.var.var1: upper(i) if length(i) > 6]
+  
+}
